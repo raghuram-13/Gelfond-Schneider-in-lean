2f7bc9ed5013dadfa5055a6ecce86b159e3658df
Karthik Dulam <karthik.06.dulam@gmail.com>
2023-09-10 13:33:10 +0530

    Changes mathlib branch to aided_by

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «gelfond-schneider»
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git"@"aided_by"
 
 @[default_target]
 lean_lib GelfondSchneider
