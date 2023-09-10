dd88c85eb66680dfe75ce2b29a409eb636a82ae2
Karthik Dulam <karthik.06.dulam@gmail.com>
2023-09-10 23:57:37 +0530

    lake update

--- GelfondSchneider/Basic.lean
+++ GelfondSchneider/Basic.lean
@@ -9,14 +9,17 @@ noncomputable
 def m : ℤ := (minpoly ℤ γ).natDegree
 def μ : ℂ → ℝ := sorry
 
-lemma algebraicElementLowerBound (A : ℤ) (hA : IsIntegral ℤ (A*γ)) (hg : γ ≠ 0) :
-    ‖γ‖ ≥ ((A:ℚ)^(-m γ): ℝ) * (μ γ)^(1 - m γ) := sorry
+example  (x y : ℝ) : (2 + 5)^2 = 49 := by#
+
+lemma algebraicElementLowerBound (hA : IsIntegral ℤ (A*γ)) (hgamma : γ ≠ 0) :
+    ‖γ‖ ≥ (A^(- m γ)) * (μ γ)^(1 - m γ) := by sorry
+
 
 open Metric
 variable (R : ℝ) (hR : 0 < R) (n : ℕ) (z : ℕ → ℂ) (G : ℂ → ℂ)
 
-noncomputable def maxOnCirc := sSup $ norm ∘ G '' {z : ℂ | ‖z‖ = R}
+noncomputable def maxOnCirc := sSup ( norm ∘ G '' {z : ℂ | ‖z‖ = R})
 
 lemma JensenLike (hG : DifferentiableOn ℂ G (closedBall 0 R)) 
     (hGz : ∀ i : Finset.range n, G (z i) = 0) :
-  ‖G 0‖ ≤ R^(-n:ℤ) * ‖Finset.prod (Finset.range n) z‖ * maxOnCirc R G := sorry
+  ‖G 0‖ ≤ R^(-n) * ‖Finset.prod (Finset.range n) z‖ * maxOnCirc R G := sorry
